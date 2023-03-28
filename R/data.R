810bf80f10c5bb194fdc1899f1688e0d5ad6be5f
JLDC <jonathan.chassot@unisg.ch>
2023-03-28 11:57:44 +0200

    Add feedback()

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' Input: message content template for feedback()
+#'
+#' This message content serves as user input for the feedback function.
+#'
+#' @format ## `feedback_input`
+#' A data frame with 2 rows and 2 columns representing the message object in API
+#' calls.
+#' @source Contributed by jldc
+#'
+"feedback_input"
+
+
+#' Input: message content template for explain_code()
+#'
+#' This message content serves as user input for the feedback function.
+#'
+#' @format ## `feedback_input`
+#' A data frame with 2 rows and 2 columns representing the message object in API
+#' calls.
+#' @source Contributed by jldc
+#'
+"explain_code_input"
+
+#' Input: message content template for compare_solutions()
+#'
+#' This message content serves as user input for the compare_solutions function.
+#'
+#' @format ## `compare_solutions_input`
+#' A data frame with 2 rows and 2 columns representing the message object in API
+#' calls.
+#' @source Contributed by jldc
+"compare_solutions_input"
