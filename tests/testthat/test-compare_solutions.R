880a7e1be814f59c3a74dfae04bf429f9c35e12b
umatter <umatter@protonmail.com>
2024-08-19 09:17:52 +0200

    test: Update file paths in compare_solutions test case

--- tests/testthat/test-compare_solutions.R
+++ tests/testthat/test-compare_solutions.R
@@ -1,8 +1,8 @@
 if (!nchar(Sys.getenv("OPENAI_API_KEY")) == 0) {
     test_that("Comparing solutions works", {
         result <- compare_solutions(
-            attempt_file = "../../inst/testing/example_attempt.R",
-            solution_file = "../../inst/testing/example_solution.R"  # Updated path
+            attempt_file = "inst/testing/example_attempt.R",
+            solution_file = "inst/testing/example_solution.R"  
         )
         expect_type(result, "character")
     })
